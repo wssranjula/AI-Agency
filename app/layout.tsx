65bf9a96a3874b1bf68a49546e99c95af3457126
Suresh Ranjula <wssranjula1@gmail.com>
2024-10-19 10:37:28 +0530

    updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,15 +6,15 @@ import { Analytics } from "@vercel/analytics/react";
 const font = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Bird | Design & Development Studio",
-  description: "Grow your business with Bird, a design & development studio.",
+  title: "Intelliflow | Make AI work for you",
+  description: "Grow your business with Intelliflow, a design & development studio.",
   openGraph: {
     images: [
       {
         url: 'https://www.google.com/maps/uv?viewerState=lb&pb=!1s0x11ae81c94242f451:0x2a6b0cac40414137!5sGlobstand+technologies&imagekey=!1e10!2sAF1QipPrTTNGxsOszQTXFD3b68e40o0V_LiVsTWwh-H6&cr=rp_35',
         width: 1200,
         height: 630,
-        alt: 'Bird Logo',
+        alt: 'Intelli flow',
       },
     ],
   },
