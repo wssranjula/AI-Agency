3c1e3305f2734432821be1550a1039b965c815d5
Suresh Ranjula <wssranjula1@gmail.com>
2024-10-19 14:02:08 +0530

    added shiny demo

--- app/page.tsx
+++ app/page.tsx
@@ -144,7 +144,7 @@ export default function Home() {
 
       <main className="md:pb-10">
         <div className="md:px-0 mx-6 xl:w-4/5 2xl:w-[68%] md:mx-auto mt-14">
-          {/* <AnimatedShinyTextDemo /> */}
+          <AnimatedShinyTextDemo />
 
           <h1>
             <CoverDemo />
--- components/demos/animated-shiny-text-demo.tsx
+++ components/demos/animated-shiny-text-demo.tsx
@@ -26,10 +26,10 @@ export function AnimatedShinyTextDemo() {
           gap-x-6">
           ⭐{" "}   ⭐{" "}   ⭐{" "}   ⭐{" "}   ⭐
           <div className="flex items-center space-x-2 py-2 md:py-0">
-          <FcGoogle className="h-6 w-6 " />
-            <span className="text-gray-600 hover:underline cursor-pointer ">
-              4.9 stars 100+ satisfied customers
-            </span>
+          {/* <FcGoogle className="h-6 w-6 " /> */}
+            {/* <span className="text-gray-600 hover:underline cursor-pointer "> */}
+            AI solutions that matter
+            {/* </span> */}
           </div>
           </div>
         </AnimatedShinyText>
